5f3f823ea4b029dcb4b569b5459306d16f55a446
Alfat1989 <gilmalf89@gmail.com>
2022-07-10 18:19:43 +0300

    add mobile menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,34 +1,25 @@
-(() => {
-  const mobileMenu = document.querySelector(".js-menu-container");
-  const openMenuBtn = document.querySelector(".js-open-menu");
-  const closeMenuBtn = document.querySelector(".js-close-menu");
-  const mobileMenuLink = document.querySelector(".mobile-link");
+const burgerMenu = document.querySelector(".header__burger");
+const menuContainer = document.querySelector(".menu-container");
+const closeMenuBtn = document.querySelector(".close-btn");
+const menuLinks = document.querySelectorAll(".mobile-link");
 
-  const closeMenu = () => {
-    // document.querySelector(".menu-container").style.display = "none";
-    return toggleMenu()
-  };
-
-  const toggleMenu = () => {
-    const isMenuOpen = openMenuBtn.getAttribute("aria-expanded") === "true" || false;
-    openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
-    mobileMenu.classList.toggle("is-open");
-
-    const scrollLockMethod = !isMenuOpen
-      ? "disableBodyScroll"
-      : "enableBodyScroll";
-    bodyScrollLock[scrollLockMethod](document.body);
-  };
-
-  openMenuBtn.addEventListener("click", toggleMenu);
-  closeMenuBtn.addEventListener("click", toggleMenu);
-  mobileMenuLink.addEventListener("click", closeMenu);
-
-  // Close the mobile menu on wider screens if the device orientation changes
-  window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove("is-open");
-    openMenuBtn.setAttribute("aria-expanded", false);
-    bodyScrollLock.enableBodyScroll(document.body);
+function onBurgerClick() {
+  menuContainer.classList.toggle("is-open");
+  const links = menuLinks.forEach((menuLink) => {
+    menuLink.addEventListener("click", onLinkClick);
   });
-})();
+}
+
+function onLinkClick(e) {
+  menuContainer.classList.toggle("is-open");
+}
+
+burgerMenu.addEventListener("click", onBurgerClick);
+closeMenuBtn.addEventListener("click", onBurgerClick);
+
+window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
+  if (!e.matches) return;
+  menuContainer.classList.remove("is-open");
+  // openMenuBtn.setAttribute("aria-expanded", false);
+  bodyScrollLock.enableBodyScroll(document.body);
+});
