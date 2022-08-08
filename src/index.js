a3a98998f1ce4c425c6c3bb5a66520656a0562fd
Alfat1989 <gilmalf89@gmail.com>
2022-08-08 16:27:48 +0300

    add metrix

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 // import './custom.scss'
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from "./js/gallery-items";
+
+const galleryEl = document.querySelector(".gallery");
+galleryEl.style.listStyle = "none";
+
+function addGalleryMarkup(gallery) {
+  return gallery
+    .map(
+      ({ original, preview, description }) =>
+        `
+        <li>
+            <a class="gallery__item" href="${original}">
+            <img class="gallery__image" 
+            src="${preview}" 
+            alt="${description}"/>
+            </a>
+        </li>
+        `
+    )
+    .join("");
+}
+
+galleryEl.innerHTML = addGalleryMarkup(galleryItems);
+
+const instance = new SimpleLightbox(".gallery a", {
+  animationSpeed: 250,
+  loop: true,
+  enableKeyboard: true,
+  preloading: true,
+  docClose: true,
+  captionsData: "alt",
+});
