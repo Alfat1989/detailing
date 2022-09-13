233f5e34986c7abd4cd2111adde633177b6f4fa8
Alfat1989 <gilmalf89@gmail.com>
2022-09-13 19:21:42 +0300

    add scroll scripts

--- src/index.js
+++ src/index.js
@@ -2,22 +2,67 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from "./js/gallery-items";
+import $ from "jquery";
 
 const galleryEl = document.querySelector(".gallery");
-const anchors = document.querySelectorAll("a[href*='#']");
-for (let anchor of anchors) {
-  if (anchor) {
-    anchor.addEventListener("click", function (e) {
-      e.preventDefault();
-      const anchorId = anchor.getAttribute("href");
-      console.log(anchorId);
-      document.querySelector(anchorId).scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
-    });
-  }
-}
+
+$("a.scroll-to").on("click", function (e) {
+  e.preventDefault();
+  var anchor = $(this).attr("href");
+  $("html, body")
+    .stop()
+    .animate(
+      {
+        scrollTop: $(anchor).offset().top - 80,
+      },
+      800
+    );
+});
+
+// const menuLinks = document.querySelectorAll(".menu__link[data-goto]");
+// if (menuLinks.length > 0) {
+//   menuLinks.forEach((menuLink) => {
+//     menuLink.addEventListener("click", onMenuLinkClick);
+//   });
+
+//   function onMenuLinkClick(e) {
+//     const menuLink = e.target;
+//     console.log(menuLink);
+//     if (
+//       menuLink.dataset.goto &&
+//       document.querySelector(menuLink.dataset.goto)
+//     ) {
+//       const gotoBlock = document.querySelector(menuLink.dataset.goto);
+//       const gotpBlockValue =
+//         gotoBlock.getBoundingClientRect().top +
+//         scrollY -
+//         document.querySelector(".header").scrollIntoView;
+//       window.scrollTo({
+//         top: gotpBlockValue,
+//         block: "start",
+//         behavior: "smooth",
+//       });
+
+//       e.preventDefault();
+//     }
+//   }
+// }
+
+// const anchors = document.querySelectorAll("a[href*='#']");
+// for (let anchor of anchors) {
+//   if (anchor) {
+//     anchor.addEventListener("click", function (e) {
+//       e.preventDefault();
+//       const anchorId = anchor.getAttribute("href");
+//       console.log(anchorId);
+//       document.querySelector(anchorId).scrollIntoView({
+//         behavior: "smooth",
+//         block: "start",
+//         top: "80px",
+//       });
+//     });
+//   }
+// }
 
 galleryEl.style.listStyle = "none";
 
@@ -49,11 +94,11 @@ const instance = new SimpleLightbox(".gallery a", {
   captionsData: "alt",
 });
 
-const scrollAnchors = (e) => {
-  e.preventDefault();
-  const anchorId = this.getAttribute("href");
-  document.querySelector(anchorId).scrollIntoView({
-    behavior: "smooth",
-    block: "start",
-  });
-};
+// const scrollAnchors = (e) => {
+//   e.preventDefault();
+//   const anchorId = this.getAttribute("href");
+//   document.querySelector(anchorId).scrollIntoView({
+//     behavior: "smooth",
+//     block: "start",
+//   });
+// };
